09d0e4b98781f097f1a65b9c31ef5d38be2ebfc7
ohmycloud <ohmycloudy@gmail.com>
2025-01-08 13:43:51 +0800

    read parquet with polars

--- ch02-reading-writing-files/read_parquet.py
+++ ch02-reading-writing-files/read_parquet.py
@@ -12,3 +12,20 @@ print(df.head())
 df.write_parquet('output.parquet', compression='lz4', compression_level=10)
 lf = pl.scan_parquet(input_path)
 lf.sink_parquet('lazy_output.parquet', maintain_order=False)
+
+# read partitioned parquet file
+input_path = '../data/venture_funding_deals_partitioned'
+df = pl.read_parquet(
+    input_path,
+    use_pyarrow=True,
+    pyarrow_options={'partitioning': 'hive'}
+)
+print(df.head())
+df.write_parquet(
+    '../data/output.parquet',
+    use_pyarrow=True,
+    pyarrow_options={
+        'partition_cols': ['Industry'],
+        'existing_data_behavior': 'overwrite_or_ignore'
+    }
+)
